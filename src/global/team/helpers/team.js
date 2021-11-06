1e232b690aff3c6d663f02a6a8d9ec8217b45ba4
g1alexander <alexlds26@gmail.com>
2021-11-06 11:35:52 -0500

    feat: UI Team

---
+++ src/global/team/helpers/team.js
@@ -0,0 +1,28 @@
+import { mdiLinkedin, mdiGithub, mdiTwitter } from "@mdi/js";
+
+export default [
+  {
+    id: 1,
+    name: "Henry Letham",
+    avatar: "https://dummyimage.com/200x200",
+    rol: "UI Developer",
+    bio: "DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.",
+    links: [
+      {
+        name: "Linkedin",
+        url: "https://linkedin.com/g1alexander/",
+        icon: mdiLinkedin,
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com/g1alexander/",
+        icon: mdiGithub,
+      },
+      {
+        name: "Twitter",
+        url: "https://twitter.com/g1alexander_/",
+        icon: mdiTwitter,
+      },
+    ],
+  },
+];
--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,12 @@ const routes = [
     ...Profile,
   },
   {
+    name: "team",
+    path: "/team",
+    component: () =>
+      import(/* webpackChunkName: "team" */ "@/global/team/Team.vue"),
+  },
+  {
     path: "/car",
     ...Car,
   },
